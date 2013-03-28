163b48a3c381648409338297defb69948459f4bd
pshon <alexey@itbc.pro>
2013-03-28 21:16:43 +0400

    Update long-message-example.js

--- long-message-example.js
+++ long-message-example.js
@@ -6,7 +6,10 @@ var smsclient = shorty.createClient('./conf/smpp.conf');
 smsclient.on('bindSuccess', function(pdu) {
   console.log('Bind success');
   var sms = new smsModel(smsclient, {sequence_id:154645, use_message_payload:false});
+  // utf-8 message
   sms.send('enotice', '79257559544', 'Hello УТФ 8!');
+  // long message
+  sms.send('enotice', '79257559544', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam');
 });
 
 smsclient.connect();
