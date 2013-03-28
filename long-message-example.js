ad2eef29dfadc9ef0c1aa8bb23e0dc0632d389f1
pshon <alexey@itbc.pro>
2013-03-28 21:02:13 +0400

    Create long-message-example.js

---
+++ long-message-example.js
@@ -0,0 +1,33 @@
+var sys = require('util');
+var shorty = require('./lib/shorty');
+var smsModel = require('./lib/sms').sms;
+var smsclient = shorty.createClient('./conf/smpp.conf');
+
+smsclient.on('bindSuccess', function(pdu) {
+  console.log('Bind success');
+  var sms = new smsModel(smsclient, {sequence_id:154645, use_message_payload:false});
+  sms.send('enotice', '79257559544', 'Hello УТФ 8!');
+});
+
+smsclient.connect();
+
+
+
+
+
+
+
+
+
+
+
+var sighandle = function() {
+    process.stdin.end();
+    smsclient.unbind();
+    process.exit();
+};
+
+process.on('SIGHUP', sighandle);
+process.on('SIGINT', sighandle);
+process.on('SIGQUIT', sighandle);
+process.on('SIGTERM', sighandle);
